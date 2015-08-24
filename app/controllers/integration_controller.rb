5f616c439131a4d007ee437d20a2d8ca34ff14b2
Максим Кузьмин <maximkuzmin>
2015-08-24 09:36:41 +0600

    slash added to timetable url

--- app/controllers/integration_controller.rb
+++ app/controllers/integration_controller.rb
@@ -2,7 +2,7 @@ class IntegrationController < ApplicationController
   require 'rest-client'
 
   def time_and_week
-    url = "#{Settings['timetable.url']}api/v1/current_week_parity"
+    url = "#{Settings['timetable.url']}/api/v1/current_week_parity"
     result = JSON.parse(RestClient.get url)
     result["current_week"] = result["current_week"] == 'odd' ? 'Нечётная' : 'Чётная'
     result[:date] = I18n.l(Time.zone.now, :format => '%d %B %Y')
